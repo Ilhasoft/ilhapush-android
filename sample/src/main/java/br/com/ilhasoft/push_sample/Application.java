72e691b609dc1f186464ce950b72177aff2ae595
John Cordeiro <john.dalton@ilhasoft.com.br>
2016-07-02 14:02:53 -0300

    Refactor sample for new registration process

--- sample/src/main/java/br/com/ilhasoft/push_sample/Application.java
+++ sample/src/main/java/br/com/ilhasoft/push_sample/Application.java
@@ -2,6 +2,7 @@ package br.com.ilhasoft.push_sample;
 
 import br.com.ilhasoft.push.IlhaPush;
 import br.com.ilhasoft.push.UiConfiguration;
+import br.com.ilhasoft.push_sample.services.PushRegistrationService;
 
 /**
  * Created by john-mac on 6/28/16.
@@ -14,7 +15,7 @@ public class Application extends android.app.Application {
 
         IlhaPush.initialize(this, "Token bb207a615b7e6fae61c80e0d75a8975bc4e54815"
                 , "4ff7908b-911e-434a-90ed-8ae62c629121"
-                , "198991767403");
+                , "198991767403", PushRegistrationService.class);
         IlhaPush.setUiConfiguration(new UiConfiguration()
                 .setIconResource(R.mipmap.ic_launcher)
                 .setTitleString("Sample Title"));
---
+++ sample/src/main/java/br/com/ilhasoft/push_sample/services/PushRegistrationService.java
@@ -0,0 +1,29 @@
+package br.com.ilhasoft.push_sample.services;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import br.com.ilhasoft.flowrunner.models.Contact;
+import br.com.ilhasoft.push.IlhaPush;
+import br.com.ilhasoft.push.services.PushRegistrationIntentService;
+
+/**
+ * Created by john-mac on 7/2/16.
+ */
+public class PushRegistrationService extends PushRegistrationIntentService {
+
+    private static final String TAG = "PushRegistrationService";
+
+    @Override
+    public void onGcmRegistered(String pushIdentity, Contact contact) {
+        contact.setName("IlhaPush Sample User");
+        contact.setEmail("sample@gmail.com");
+
+        try {
+            IlhaPush.updateContact(contact);
+        } catch (IOException e) {
+            Log.e(TAG, "onGcmRegistered: ", e);
+        }
+    }
+}
