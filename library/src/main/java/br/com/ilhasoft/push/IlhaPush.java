78ceb2a6574c4e8f5a59481054278c65e0643fc1
Felipe Oliveira <felipe.oliveiradev@gmail.com>
2016-08-17 16:01:17 -0300

    overload initialize method

--- library/src/main/java/br/com/ilhasoft/push/IlhaPush.java
+++ library/src/main/java/br/com/ilhasoft/push/IlhaPush.java
@@ -54,6 +54,32 @@ public class IlhaPush {
         registerGcmIfNeeded();
     }
 
+    public static void initialize(Context context, String token, String channel, String gcmSenderId) {
+        IlhaPush.context = context;
+        IlhaPush.token = token;
+        IlhaPush.channel = channel;
+        IlhaPush.gcmSenderId = gcmSenderId;
+        IlhaPush.registrationServiceClass = PushRegistrationIntentService.class;
+        IlhaPush.preferences = new Preferences(context);
+        IlhaPush.services = new RapidProServices(getToken());
+        IlhaPush.uiConfiguration = new UiConfiguration();
+
+        registerGcmIfNeeded();
+    }
+
+    public static void initialize(Context context, String token, String channel, String gcmSenderId, UiConfiguration uiConfiguration) {
+        IlhaPush.context = context;
+        IlhaPush.token = token;
+        IlhaPush.channel = channel;
+        IlhaPush.gcmSenderId = gcmSenderId;
+        IlhaPush.registrationServiceClass = PushRegistrationIntentService.class;
+        IlhaPush.preferences = new Preferences(context);
+        IlhaPush.services = new RapidProServices(getToken());
+        IlhaPush.uiConfiguration = uiConfiguration;
+
+        registerGcmIfNeeded();
+    }
+
     public static UiConfiguration getUiConfiguration() {
         return uiConfiguration;
     }
